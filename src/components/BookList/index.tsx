5dd23799a5b725e18d0e633d80ce1785df5aec12
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-07-25 22:03:43 -0300

    feat: books list component

---
+++ src/components/BookList/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+type Props = {
+  books: { name: string }[];
+};
+
+const BookList = ({ books }: Props) => {
+  return (
+    <div data-test='book-list'>
+      {books.map((book) => (
+        <div className='book-item'>
+          <Typography variant='h5' component='h5' className='titles'>
+            {book.name}
+          </Typography>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
