5c89163936c5e99a4246f267a525b826a32a1035
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-07-25 22:20:54 -0300

    feat: mock api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
 import BookList from "./components/BookList";
+import { api } from "./services/api";
 
 function App() {
-  const books = [
-    { name: "Refactoring" },
-    { name: "Domain-driven design" },
-  ];
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async (): Promise<void> => {
+      const { data } = await api.get("books");
+
+      setBooks(data);
+    };
+
+    getBooks();
+  }, []);
 
   return (
     <>
