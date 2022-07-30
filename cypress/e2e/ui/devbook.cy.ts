9aa985083d41d82f679ce71c029543fca0238126
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-07-30 16:00:45 -0300

    test: axios request on test

--- cypress/e2e/ui/devbook.cy.ts
+++ cypress/e2e/ui/devbook.cy.ts
@@ -1,21 +1,52 @@
+import { api } from "./../../../src/services/api";
+
 describe("Devbook application", () => {
-  beforeEach(() => {
-    const baseUrl = "http://localhost:3000/";
-    cy.visit(baseUrl);
+  before(async () => {
+    return await api
+      .delete("books?_cleanup=true")
+      .catch((err: any) => err);
+  });
+
+  beforeEach(async () => {
+    const books = [
+      { id: 1, name: "Refactoring" },
+      { id: 2, name: "Domain-driven design" },
+      { id: 3, name: "Test-driven design" },
+    ];
+
+    return Promise.all(
+      books.map((book) =>
+        api.post("books", book, {
+          headers: { "content-type": "application/json" },
+        })
+      )
+    );
+  });
+
+  afterEach(async () => {
+    return await api
+      .delete("books?_cleanup=true")
+      .catch((err: any) => err);
   });
 
   it("Visits the DevBook page", () => {
+    const baseUrl = "http://localhost:3000/";
+    cy.visit(baseUrl);
+
     //? Pega o valor de um componente
     cy.get('h2[data-test="heading"]').contains("DevBook!");
   });
 
   it("Show's a book list", () => {
+    const baseUrl = "http://localhost:3000/";
+    cy.visit(baseUrl);
+
     //? Verificar se existe algo renderizado
     cy.get('div[data-test="book-list"]').should("exist");
 
     cy.get("div.book-item").should((books) => {
       //? Ao menos 2 divs com a classe book-item
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
 
       //? Pega os títulos dos livros
       const titles = [...books].map(
@@ -26,6 +57,7 @@ describe("Devbook application", () => {
       expect(titles).to.deep.equal([
         "Refactoring",
         "Domain-driven design",
+        "Test-driven design",
       ]);
     });
   });
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:4200" });
+export const api = axios.create({
+  baseURL: "http://localhost:4200",
+});
